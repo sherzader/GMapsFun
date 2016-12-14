0a72a3dcb532dc840b7071fba22fcd7bed352036
Christine Liao <astridcliao@gmail.com>
2016-12-14 15:16:23 -0800

    connected to google maps

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TerritoryMap from './map';
 
 const App = () => (
   <div>
     <h1>Marked</h1>
     <p>For dogs to keep track of their territory</p>
+    <TerritoryMap />
   </div>
 );
 
---
+++ client/components/map.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+class TerritoryMap extends React.Component{
+  componentDidMount(){
+    let map;
+    let service;
+    let infoWindow;
+    let mapNode = this.refs.map;
+    const sf = {lat: 37.7758, lng: -122.435};
+    const mapOptions = {
+      center: sf,
+      zoom: 13
+    }
+
+    map = new google.maps.Map(mapNode, mapOptions);
+
+     let request = {
+       location: sf,
+       radius: '500',
+       type: ['park']
+     };
+
+     infoWindow = new google.maps.InfoWindow();
+
+     service = new google.maps.places.PlacesService(map);
+     service.nearbySearch(request, callback);
+
+
+    function callback(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+      }
+    }
+
+    function createMarker(place) {
+      let placeLoc = place.geometry.location;
+      let marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
+
+      google.maps.event.addListener(marker, 'click', () => {
+        console.log(place.name);
+      });
+    }
+
+  }
+
+  render(){
+    const mapHeight = {
+      height: '300px'
+    };
+
+    return (
+      <div>
+        <div id='map-container' ref='map' style={mapHeight}></div>
+      </div>
+    )
+  }
+}
+
+export default TerritoryMap;
