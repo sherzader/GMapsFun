5c8f705db2b2a9f734ba81f97bda31c863958588
Christine Liao <astridcliao@gmail.com>
2016-12-14 11:55:54 -0800

    implementing provider

---
+++ client/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Marked</h1>
+    <p>For dogs to keep track of their territory</p>
+  </div>
+);
+
+export default App;
--- client/marked.jsx
+++ client/marked.jsx
@@ -5,6 +5,6 @@ import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store = configureStore();
-  const root = document.getElementByID('content');
+  const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 })
