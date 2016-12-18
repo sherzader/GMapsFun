3be12c5f538d379a86f478be339aa4afb749e95d
Christine Liao <astridcliao@gmail.com>
2016-12-18 10:38:32 -0800

    still trying to fix this bug in toggleMark

--- client/components/TodoList.jsx
+++ client/components/TodoList.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 class TodoList extends React.Component{
   toggleMark(e){
-    let target = e.target;
+    e.preventDefault();
+    let target, currentClass, i, name, marked;
+    target = e.target;
     console.log(target);
-    let currentClass = e.target.className;
+    currentClass = e.target.className;
     target.className = (currentClass === 'paw-print-unmarked') ? 'paw-print-marked' : 'paw-print-unmarked';
-    let i = target.getAttribute('data-index');
-    let name = target.getAttribute('data-name');
-    let marked = target.getAttribute('data-mark');
-    console.log(i, name, target.className);
-    this.props.markTerritory(name, target.className, i);
+    i = target.getAttribute('data-index');
+    name = target.getAttribute('data-name');
+    marked = target.getAttribute('data-mark');
+    this.props.markTerritory(name, marked, i);
+    console.log(i, name, marked);
+
   }
 
   removeTerritory(index){
@@ -23,8 +26,9 @@ class TodoList extends React.Component{
         <h3>Territories to mark</h3>
         <ul>
           {this.props.territories.map((territory, index) => {
+            let print = (territory.marked) ? 'paw-print-marked' : 'paw-print-unmarked';
             return <li key={index}>{territory.name}
-              <img data-index={index} data-name={territory.name} data-mark={territory.marked} className="paw-print-unmarked" onClick={this.toggleMark.bind(this)} src="https://www.thesage.com/images/PawPrint.png" />
+              <img data-index={index} data-name={territory.name} data-mark={territory.marked} className={print} onClick={this.toggleMark.bind(this)} src="https://www.thesage.com/images/PawPrint.png" />
               <button onClick={this.removeTerritory.bind(this, index)}>X</button>
             </li>
             })
