e4df411b2dca38cd827a58cccceece010a180f38
Christine Liao <astridcliao@gmail.com>
2017-01-13 23:04:15 -0800

    minor changes

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import TerritoryMap from './map';
 import TodoList from './TodoList';
-import DoneList from './DoneList';
 
 const Search = ({territories, addTerritory, addAndMark, removeTerritory, toggleMark}) => (
       <div>
