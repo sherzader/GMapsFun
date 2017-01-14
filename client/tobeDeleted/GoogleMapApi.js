5f93520173543ab1a59377c5389a2e76f63844c9
Christine Liao <astridcliao@gmail.com>
2017-01-13 23:00:23 -0800

    deleted unnecessary files

--- client/tobeDeleted/GoogleMapApi.js
+++
@@ -1,7 +0,0 @@
-import { GoogleApiWrapper } from 'google-maps-react';
-import MapWrapper from './MapWrapper';
-
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDH4E1rJ4tVn38eqWRBOdiM9ksQDLEPfpc',
-  libraries: ['places']
-})(MapWrapper);
