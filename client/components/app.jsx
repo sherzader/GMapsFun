d28548282375a10f455c3d742c40753bede8b620
Christine Liao <astridcliao@gmail.com>
2017-01-16 22:56:10 -0800

    adding paw-print to header

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,7 +3,9 @@ import SearchContainer from './SearchContainer';
 
 const App = () => (
   <div>
-    <h1 className='center'>Marked</h1>
+    <h1 className='center'>Marked
+      <img className="paw-print-marked"
+            src="https://www.thesage.com/images/PawPrint.png" /></h1>
     <p className='center'>For dogs to keep track of their territory</p>
     <SearchContainer />
   </div>
